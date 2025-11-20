c3112bb2ad0748dc962f5ecc498c3a6bdf951f68
Klaus Meyer <spam@klaus-meyer.net>
2025-11-20 20:56:29 +0100

    upgrade k8s nodes to debian trixie (13) (#2)

---
+++ roles/kubernetes/files/bin/update-master.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+
+set -e
+
+version="1.34.2"
+
+apt-mark unhold kubeadm
+apt-get update
+apt-get install -y kubeadm="${version}-*"
+apt-mark hold kubeadm
+
+kubeadm version
+kubeadm upgrade plan
+kubeadm upgrade apply "v${version}"
+
+apt-mark unhold kubelet kubectl
+apt-get update
+apt-get install -y kubelet="${version}-*" kubectl="${version}-*"
+apt-mark hold kubelet kubectl
+
+systemctl daemon-reload
+systemctl restart kubelet
